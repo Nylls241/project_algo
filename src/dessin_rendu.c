e79435f490d4b6e269bccef91f75d1c2b07971c4
Bakagamin <96483294+Bakagamin@users.noreply.github.com>
2022-12-31 17:16:52 +0100

    changement des entêtes des fonctions

--- src/dessin_rendu.c
+++ src/dessin_rendu.c
@@ -1,12 +1,14 @@
 #include "../include/include.h"
 
-void prepareScene(void)
+application prepareScene(application app)
 {
 	SDL_SetRenderDrawColor(app.rendu, 96, 128, 255, 255);
 	SDL_RenderClear(app.rendu);
+	
+	return app;
 }
 
-void presentScene(void)
+void presentScene(application app)
 {
 	SDL_RenderPresent(app.rendu);
 }
