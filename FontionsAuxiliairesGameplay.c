48e7c8a31c0c7f48b66ed16904f4fc9836f9baae
Bakagamin <froment.guillaume.91@gmail.com>
2022-12-31 16:23:41 +0100

    suppression des structures redondantes

--- FontionsAuxiliairesGameplay.c
+++ FontionsAuxiliairesGameplay.c
@@ -10,27 +10,6 @@ Les codes suivants sont pour l'instant des algorithmes qui nous permettent de d�
 //Structures
 int reserve [15] [4]; //permet de "stocker" les tuiles, numéro, puis couleur
 
-typedef struct tuile{
-    int valeur; //valeur numérique de la tuile, le 14 est le joker
-    int couleur; //couleur de la tuile : 1 = rouge, 2 = noir, 3 = orange, 4 = bleu
-} tuile;
-
-typedef struct combinaison{
-    tuile list [14]; //permet de stocker toutes les tuiles de la combinaison
-    int isValid; //permet de stocker la validité de la combinaison, qui est nécessaire pour la poser sur le chevalet
-} combinaison;
-
-//Seul un joueur peut voir son propre chevalet
-typedef struct chevalet{
-    int idJoueur; //id du joueur qui possède le chevalet
-    tuile list [30]; //contient les tuiles du chevalet
-} chevalet;
-
-//tous les joueurs peuvent voir le plateau de jeu
-typedef struct plateau{
-    combinaison list [40]; // permet de stocker les combinaisons à afficher
-} plateau;
-
 //Score 
 
 int* initScore(int scores[]){
