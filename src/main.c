bc3c0fa4f7f1ce6d02e5e714353e166c14f8aecd
Nylls_Gersan <boutotonyllsgersan@gmail.com>
2023-01-22 06:47:27 +0100

    update main.c

--- src/main.c
+++ src/main.c
@@ -4,7 +4,6 @@ int main(void)
 {
  	application app = initSDL() ;
 	prepareScene(app) ;
-	app = dessiner_plateau(app) ; 
 	presentScene(app) ; 
 	SDL_Delay(16) ;/* Cette attente sert à limiter notre boucle à environ 62 images par seconde et empêche également l'application de tourner à plein régime et de consommer beaucoup trop de temps du CPU.*/
 	SDL_Delay(6000) ;//input() ;
