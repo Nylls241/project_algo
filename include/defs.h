ff5f7e84208cad1ac271d89748f17753f5757575
Bakagamin <froment.guillaume.91@gmail.com>
2023-01-24 12:41:20 +0100

    update defs

--- include/defs.h
+++ include/defs.h
@@ -7,7 +7,9 @@
 #define hauteur_tuile 50 
 #define position_plateau_x 0
 #define position_plateau_y 0
-#define hauteur_chevalet 100 ;
-#define largeur_chevalet 827;
+#define hauteur_chevalet 100
+#define largeur_chevalet 645
+#define position_chevaletx 131
+#define position_chevalety 620 
 
 #endif
