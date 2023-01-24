d31de808014912c22e38d6df3e21c1842d439f23
Sala <Salambaye2001@gmail.com>
2023-01-24 14:17:13 +0100

    Fichier modifié

--- src/menu.c
+++ src/menu.c
@@ -1,22 +1,33 @@
-//#include <SDL.h>
-#include <stdio.h>  
-#include <stdlib.h> 
+#include "SDL.h"  
+#include <stdio.h>
+#include <stdlib.h>
 
-/*void SDL_ExitWithError(const char *message);
+void SDL_ExitWithError(const char *message);
 
-int main(int argc, char **argv)
+int setWindowColor(SDL_Renderer *renderer, SDL_Color color)
+{
+    if(SDL_SetRenderDrawColor(renderer, color.r, color.g, color.b) < 0)
+        return -1;
+    if(SDL_RenderClear(renderer) < 0)
+        return -1;
+    return 0;  
+}
+
+int main(void)
 {
 
-	SDL_version nb;
-	SDL_VERSION(&nb);
-	printf("Bienvenue sur la SDL %d.%d.%d !\n", nb.major, nb.minor, nb.patch );
 
 	SDL_Window *window = NULL;
 	SDL_Renderer *renderer = NULL;
+	SDL_Color blanc = {255, 255, 255};
+
+
+    setWindowColor(renderer, blanc); 
+   
 
 	if(SDL_Init(SDL_INIT_VIDEO) != 0)
 	{
-		SDL_Log("Erruer d'initialisation de la SDL > %s\n", SDL_GetError());
+		SDL_Log("Erreur d'initialisation de la SDL > %s\n", SDL_GetError());
 		exit(EXIT_FAILURE);
 	}
 
@@ -25,7 +36,7 @@ int main(int argc, char **argv)
 	window = SDL_CreateWindow("Première fênêtre SDL",                             // Titre de la fenetre
 														SDL_WINDOWPOS_CENTERED,   //abscisse (x)
 														SDL_WINDOWPOS_CENTERED,   // ordonnee (y)
-														800, 600,                 // largeur et hauteur de la fénétre en pixels
+														400, 300,                 // largeur et hauteur de la fénétre en pixels
 														SDL_WINDOW_SHOWN          //Drapeau : on peut aussi mettre 0 par défaut        
 														); 
 
@@ -48,71 +59,51 @@ int main(int argc, char **argv)
 		
 	}
 
-	SDL_RenderPresent(renderer);
 	if(SDL_RenderClear(renderer) != 0)    //Qui affiche le rendu de notre fênêtre : il renvoie 0 si elle réussit et une valeur négative sinon
 	{
 		SDL_ExitWithError("Erreur : Effacement du rendu échoué!");
-	}  */
-
-
-
-
-
-
+	}  
 	
 /*--------------------------------------------------------------------*/
-	//SDL_Delay(5000); //Pour maintenir la fênêtre pendant 5 secondes
+if (SDL_SetRenderDrawColor(renderer, 255, 255, 255, SDL_ALPHA_OPAQUE ) != 0) // 255, 255, 255 désigne la couleur blanche, SDL_ALPHA_OPAQUE permet d'afficher la couleur
+{
+	SDL_ExitWithError("Impossible de changer la couleur pour le rendu!");
+}
+
+/*if (SDL_SetRenderDrawPoint(renderer, 100, 150) != 0)
+{
+	SDL_ExitWithError("Impossible de dessiner un point!");
+}
+if (SDL_SetRenderDrawPoint(renderer, 50, 50, 200, 200) != 0)
+{
+	SDL_ExitWithError("Impossible de dessiner une ligne!");
+}
+if (SDL_SetRenderDrawPoint(renderer, &rectangle) != 0)
+{
+	SDL_ExitWithError("Impossible de dessiner un rectangle!");
+}
+SDL_Rect rectangle;
+rectangle.x = 200;
+rectangle.y = 100;
+rectangle.w = 150; // largeur
+rectangle.h = 120; // hauteur
+*/
+	SDL_RenderPresent(renderer);
+	SDL_Delay(5000); //Pour maintenir la fênêtre pendant 5 secondes
 /*--------------------------------------------------------------------*/
 
-	/*SDL_DestroyRenderer(renderer); //Pour détruire le rendu
+	SDL_DestroyRenderer(renderer); //Pour détruire le rendu
 	SDL_DestroyWindow(window);     //Pour détruire la fênêtre
     SDL_Quit();
 
-	return EXIT_SUCCESS; //return 0;
-}*/
+	return 0; //return 0;
+}
 
-/*void SDL_ExitWithError(const char *message)
+void SDL_ExitWithError(const char *message)
 {
 	SDL_Log("Erreur : %s > %s\n", message, SDL_GetError);
 	SDL_Quit();
 	exit(EXIT_FAILURE);
-} */
+} 
 
-int main(int argc, char const *argv[])
-{
-	int choix, nbre;
-	printf("\n\n");
-	printf("---------------Menu du jeu Rummikub------------------------\n");
-	printf("--> 1) Choisir le nombre de joueurs \n");
-	printf("--> 2) Commencer une nouvelle partie\n");
-	printf("--> 3) Saisir les noms des joueurs\n");
-    printf("Entrez un choix : \n");
-    scanf("%d",&choix);
 
-    switch(choix)
-    {
-    	case 1 :  
-    		printf("Choisissez le nombre de joueurs : entre 2 et 4\n");
-    		
-
-    		while(nbre < 2 && nbre > 4)
-    		{
-    			scanf("%d\n", &nbre);
-       			printf("Le nombre de joueurs doit etre compris entre 2 et 4\n");
-        		printf("Veuillez saisir %c nouveau le nombre de joueurs\n", 133);
-        		scanf("%d\n", &nbre);
-        	}
-    }
-    		break;
-    	case 2 :
-    		break;
-    	case 3 :
-    		break;
-    	default :
-    		break;
-    }
-
-    
-    system("pause");
-    return 0;
-}
