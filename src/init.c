ce3e16855070d7cd4295e5f425ba68430326deaa
Nylls_Gersan <boutotonyllsgersan@gmail.com>
2023-02-02 14:48:34 +0100

    update

--- src/init.c
+++ src/init.c
@@ -71,4 +71,14 @@ int SDL_ErrorQuit(const char *message)
 
 }
 
+application implemente_rummikub(void){
+		// Implentation du Rummikub 
+	application app = dessin_tuile(app,14);//  on pioche 14 tuile au debut de la partie 
+
+	presentScene(app) ; //le jeu est lancé 
+
+	SDL_Delay(16) ;/* Cette attente sert à limiter notre boucle à environ 62 images par seconde et empêche également l'application de tourner à plein régime et de consommer beaucoup trop de temps du CPU.*/
+	input() ; //la fenêtre du jeu est lancé
+	destroySDL(app);
+}	
 //Rediger par Nylls Gersan BOUTOTO
--- src/main.c
+++ src/main.c
@@ -3,12 +3,5 @@
 int main(void) 
 {
 	
-	application app = dessin_tuile(app,14);//  on pioche 14 tuile au debut de la partie 
-
-	presentScene(app) ; //le jeu est lancé 
-
-	SDL_Delay(16) ;/* Cette attente sert à limiter notre boucle à environ 62 images par seconde et empêche également l'application de tourner à plein régime et de consommer beaucoup trop de temps du CPU.*/
-	input() ; //la fenêtre du jeu est lancé
-	destroySDL(app);
 	return 0 ;
 }
