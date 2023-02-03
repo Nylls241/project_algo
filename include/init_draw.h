c05cf53e4190fa8e64306ac7a75cd25049509fcf
Nylls_Gersan <boutotonyllsgersan@gmail.com>
2023-02-03 12:39:12 +0100

    update init_draw.h ajout des fichiers d'entêtes de display_text.c

--- include/init_draw.h
+++ include/init_draw.h
@@ -16,6 +16,19 @@ application dessin_tuile(application app,int nb);
 application implemente_rummikub() ; 
 
 
+void texture_to_rect(SDL_Renderer *rendu, SDL_Texture *texture, SDL_Rect *rectangle) ; 
+void afficher_image (char *chemin, SDL_Rect *rectangle, SDL_Renderer *rendu); 
+void menu_joueur(SDL_Renderer *rendu);
+void accueil(SDL_Renderer *rendu); 
+void afficher_texte(SDL_Renderer *rendu, char *texte, int x, int y);
+int string_length(const char* str);
+char* string_concat(const char* str1, const char* str2);
+void modifier_pseudo(int *cas,char lettre,char *pseudo1, char *pseudo2, char *pseudo3, char *pseudo4);
+void move_last_caracter(char *mot);
+void move_last_caracter_pseudo(int *cas, char *pseudo1, char *pseudo2, char *pseudo3, char *pseudo4);
+char *execution();
+
+
 
 //int reserve [15] [5]; //permet de "stocker" les tuiles, numéro, puis couleur
 int* initScore(int scores[]);
